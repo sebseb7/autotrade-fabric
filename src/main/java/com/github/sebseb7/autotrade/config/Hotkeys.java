f9b79f49ed5c5561d11133f9ea1387c0a4a388a5
sebseb7 <sebgreenbus@gmail.com>
2023-07-07 17:06:18 +0200

    right-shift+t default for opening the gui

--- src/main/java/com/github/sebseb7/autotrade/config/Hotkeys.java
+++ src/main/java/com/github/sebseb7/autotrade/config/Hotkeys.java
@@ -15,7 +15,8 @@ public class Hotkeys {
 			"Sets the input (item to sell) container");
 	public static final ConfigHotkey SET_OUTPUT_KEY = new ConfigHotkey("setOutputContainer", "",
 			"Sets the output (item to buy) container");
-	public static final ConfigHotkey OPEN_GUI_SETTINGS = new ConfigHotkey("openGuiSettings", "", "Open the Config GUI");
+	public static final ConfigHotkey OPEN_GUI_SETTINGS = new ConfigHotkey("openGuiSettings", "RIGHT_SHIFT,T",
+			"Open the Config GUI");
 
 	public static final List<ConfigHotkey> HOTKEY_LIST = ImmutableList.of(TOGGLE_KEY, SET_SELL_KEY, SET_BUY_KEY,
 			SET_INPUT_KEY, SET_OUTPUT_KEY, OPEN_GUI_SETTINGS);
